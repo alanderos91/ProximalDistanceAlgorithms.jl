a4ff28a8bcac4c52f518668d20d33ae291926824
AlfonsoLanderos <alanderos@ucla.edu>
2020-02-13 15:33:35 -0800

    Complete linear operators derived from fusion matrix

--- src/convex_regression/linear_operators.jl
+++ src/convex_regression/linear_operators.jl
@@ -1,3 +1,22 @@
+function apply_D!(C, θ)
+   for j in eachindex(θ), i in eachindex(θ)
+      C[i,j] = θ[j] - θ[i]
+   end
+
+   return C
+end
+
+function apply_Dt!(u, C)
+   fill!(u, 0)
+
+   for j in eachindex(u), i in eachindex(u)
+      u[i] -= C[i,j] # accumulate C*1
+      u[i] += C[j,i] # accumulate C'*1
+   end
+
+   return u
+end
+
 function apply_DtD!(u, θ)
    n = length(θ)
    μ = sum(θ)
--- test/convex_regression.jl
+++ test/convex_regression.jl
@@ -1,4 +1,4 @@
-import ProximalDistanceAlgorithms: apply_DtD!
+import ProximalDistanceAlgorithms: apply_D!, apply_Dt!, apply_DtD!
 
 function make_D(n)
     D = spzeros(Int, n*n, n)
@@ -16,21 +16,37 @@ function make_D(n)
 end
 
 @testset "linear operators" begin
-    @testset "Dt*D*θ" begin
-        for n in (3, 10, 100)
-            D = make_D(n)
-            DtD = D'D
-            θ = rand(n)
-            u = similar(θ)
-            v = similar(θ)
+    sample_size = (3, 10, 100)
 
-            # expected
-            mul!(u, DtD, θ)
+    for n in sample_size
+        # linear operators as matrices
+        D = make_D(n)
+        Dt = transpose(D)
+        DtD = D'D
 
-            # observed
-            apply_DtD!(v, θ)
+        # test inputs
+        θ = rand(n)
+        C = rand(n,n)
 
-            @test u ≈ v
-        end
+        # test outputs
+        u = similar(θ)
+        v = similar(θ)
+        w = zeros(n*n)
+        W = similar(C)
+
+        # test: D*θ
+        mul!(w, D, θ)   # expected
+        apply_D!(W, θ)  # observed
+        @test w ≈ vec(W)
+
+        # test: Dt*vec(C)
+        mul!(u, Dt, vec(C)) # expected
+        apply_Dt!(v, C)     # observed
+        @test u ≈ v
+
+        # test: DtD*θ
+        mul!(u, DtD, θ)   # expected
+        apply_DtD!(v, θ)  # observed
+        @test u ≈ v
     end
 end
