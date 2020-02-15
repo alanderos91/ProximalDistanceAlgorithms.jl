8ccf9abedf945c660757f32c17c04b47230b40f6
AlfonsoLanderos <alanderos@ucla.edu>
2020-02-14 20:27:23 -0800

    Add Convex and SCS solver

--- src/ProximalDistanceAlgorithms.jl
+++ src/ProximalDistanceAlgorithms.jl
@@ -1,9 +1,12 @@
 module ProximalDistanceAlgorithms
 
 using LinearAlgebra
+using Convex
 
 include(joinpath("convex_regression", "linear_operators.jl"))
 include(joinpath("convex_regression", "proxdist.jl"))
+include(joinpath("convex_regression", "convex_wrapper.jl"))
+
 export cvxreg_fit
 
 end # module
---
+++ src/convex_regression/convex_wrapper.jl
@@ -0,0 +1,21 @@
+function build_cvxreg_problem(y, X)
+    d, n = size(X)
+
+    # make problem variables
+    θ = Variable(n)
+    ξ = Variable(d, n)
+
+    # build objective
+    loss = 0.5 * (sumsquares(θ) - 2*dot(y, θ) + sumsquares(Constant(y)))
+
+    # define problem
+    problem = minimize(loss)
+
+    # add constraints
+    for j in 1:n, i in 1:n
+        constraint = θ[j] - θ[i] - dot(X[:,i] + X[:,j], ξ[:,j]) ≤ 0
+        problem.constraints += constraint
+    end
+
+    return θ, ξ, problem
+end
