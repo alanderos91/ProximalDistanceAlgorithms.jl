fce28b4d53934c976cf3e09d90fcd87c1db082e2
AlfonsoLanderos <alanderos@ucla.edu>
2020-07-03 19:27:47 -0700

    Avoid SIMD in this problem

--- src/image_denoising/operators.jl
+++ src/image_denoising/operators.jl
@@ -27,14 +27,14 @@ function LinearMaps.A_mul_B!(z::AbstractVector, D::ImgTvdFM, x::AbstractVector)
 
     # compute derivatives along columns
     for j in 1:n
-        @simd for i in 1:m-1
+        for i in 1:m-1
             @inbounds z[dxind[i,j]] = x[xind[i+1,j]] - x[xind[i,j]]
         end
     end
 
     # compute derivatives along rows
     for j in 1:n-1
-        @simd for i in 1:m
+        for i in 1:m
             @inbounds z[dyind[i,j]+offset] = x[xind[i,j+1]] - x[xind[i,j]]
         end
     end
@@ -55,32 +55,32 @@ function LinearMaps.At_mul_B!(x::AbstractVector, D::ImgTvdFM, z::AbstractVector)
     fill!(x, 0)
 
     # apply Dx'
-    @simd for j in 1:n
+    for j in 1:n
         @inbounds x[xind[1,j]] += -z[dxind[1,j]]
     end
 
     for j in 1:n
-        @simd for i in 2:m-1
+        for i in 2:m-1
             @inbounds x[xind[i,j]] += z[dxind[i-1,j]] - z[dxind[i,j]]
         end
     end
 
-    @simd for j in 1:n
+    for j in 1:n
         @inbounds x[xind[m,j]] += z[dxind[m-1,j]]
     end
 
     # apply Dy'
-    @simd for i in 1:m
+    for i in 1:m
         @inbounds x[xind[i,1]] += -z[dyind[i,1]+offset]
     end
 
     for j in 2:n-1
-        @simd for i in 1:m
+        for i in 1:m
             @inbounds x[xind[i,j]] += z[dyind[i,j-1]+offset] - z[dyind[i,j]+offset]
         end
     end
 
-    @simd for i in 1:m
+    for i in 1:m
         @inbounds x[xind[i,n]] += z[dyind[i,n-1]+offset]
     end
 
@@ -141,7 +141,7 @@ function LinearMaps.A_mul_B!(y::AbstractVector, DtD::ImgTvdFGM, x::AbstractVecto
         y[k] = x[k] - x[k+1]
 
         # intermediate rows in block
-        @simd for i in 2:m-1
+        for i in 2:m-1
             k = imind[i,j]
             y[k] = -x[k-1] + 2*x[k] - x[k+1]
         end
@@ -154,21 +154,21 @@ function LinearMaps.A_mul_B!(y::AbstractVector, DtD::ImgTvdFGM, x::AbstractVecto
     # accumulate Dy'dy
 
     # first block
-    @inbounds @simd for i in 1:m
+    @inbounds for i in 1:m
         k = imind[i,1]
         y[k] += x[k] - x[k+m]
     end
 
     # intermediate blocks
     @inbounds for j in 2:n-1
-        @simd for i in 1:m
+        for i in 1:m
             k = imind[i,j]
             y[k] += -x[k-m] + 2*x[k] - x[k+m]
         end
     end
 
     # last block
-    @inbounds @simd for i in 1:m
+    @inbounds for i in 1:m
         k = imind[i,n]
         y[k] += -x[k-m] + x[k]
     end
