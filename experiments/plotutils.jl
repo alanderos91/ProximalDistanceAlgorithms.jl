6b360ac80446f08d6bf12a81fe7fc53ede3153a5
AlfonsoLanderos <alanderos@ucla.edu>
2020-07-08 07:31:42 -0700

    Update demos

--- experiments/plotutils.jl
+++ experiments/plotutils.jl
@@ -14,7 +14,7 @@ function plot_summary(trace)
     plot!(panel2, xs, ys, scale = :log10, label = "loss")
 
     ys = trace.objective .+ eps()
-    plot!(panel2, xs, ys, scale = :log10, label = "objective")
+    plot!(panel2, xs, ys, scale = :log10, label = "objective", linestyle = :dot)
 
     # panel 3: log-log plot, norm(gradient) vs rho
     panel3 = plot(xlabel = "rho", ylabel = "norm(gradient)")
@@ -28,79 +28,53 @@ function plot_summary(trace)
     return figure
 end
 
-function table_summary(MM_trace, SD_trace, ADMM_trace; ls=true)
-    if ls
-        algorithm = ["MM+LSQR", "MM+CG", "SD", "ADMM+LSQR", "ADMM+CG"]
+function append_data!(container, trace)
+    push!(container[1], trace.iteration[end])
+    push!(container[2], trace.loss[end])
+    push!(container[3], trace.distance[end])
+    push!(container[4], trace.objective[end])
+    push!(container[5], trace.gradient[end])
+    return nothing
+end
 
-        iterations = [
-            MM_trace.lsqr.iteration[end], MM_trace.cg.iteration[end],
-            SD_trace.iteration[end],
-            ADMM_trace.lsqr.iteration[end], ADMM_trace.cg.iteration[end]
-        ]
+function table_summary(traces...; algname=nothing)
+    algorithm = String[]
+    container = [Int[], Float64[], Float64[], Float64[], Float64[]]
+    for (k, trace) in enumerate(traces)
+        if hasfield(typeof(trace), :lsqr)
+            if algname === nothing
+                push!(algorithm, "Algorithm $(k) + LSQR")
+            else
+                push!(algorithm, "$(algname[k]) + LSQR")
+            end
+            append_data!(container, trace.lsqr)
+        end
 
-        loss = [
-            MM_trace.lsqr.loss[end], MM_trace.cg.loss[end],
-            SD_trace.loss[end],
-            ADMM_trace.lsqr.loss[end], ADMM_trace.cg.loss[end]
-        ]
+        if hasfield(typeof(trace), :cg)
+            if algname === nothing
+                push!(algorithm, "Algorithm $(k) + CG")
+            else
+                push!(algorithm, "$(algname[k]) + CG")
+            end
+            append_data!(container, trace.cg)
+        end
 
-        distance = [
-            MM_trace.lsqr.distance[end], MM_trace.cg.distance[end],
-            SD_trace.distance[end],
-            ADMM_trace.lsqr.distance[end], ADMM_trace.cg.distance[end]
-        ]
-
-        objective = [
-            MM_trace.lsqr.objective[end], MM_trace.cg.objective[end],
-            SD_trace.objective[end],
-            ADMM_trace.lsqr.objective[end], ADMM_trace.cg.objective[end]
-        ]
-
-        gradient = [
-            MM_trace.lsqr.gradient[end], MM_trace.cg.gradient[end],
-            SD_trace.gradient[end],
-            ADMM_trace.lsqr.gradient[end], ADMM_trace.cg.gradient[end]
-        ]
-    else
-        algorithm = ["MM", "SD", "ADMM"]
-
-        iterations = [
-            MM_trace.iteration[end],
-            SD_trace.iteration[end],
-            ADMM_trace.iteration[end],
-        ]
-
-        loss = [
-            MM_trace.loss[end],
-            SD_trace.loss[end],
-            ADMM_trace.loss[end],
-        ]
-
-        distance = [
-            MM_trace.distance[end],
-            SD_trace.distance[end],
-            ADMM_trace.distance[end],
-        ]
-
-        objective = [
-            MM_trace.objective[end],
-            SD_trace.objective[end],
-            ADMM_trace.objective[end],
-        ]
-
-        gradient = [
-            MM_trace.gradient[end],
-            SD_trace.gradient[end],
-            ADMM_trace.gradient[end],
-        ]
+        if hasfield(typeof(trace), :iteration)
+            if algname === nothing
+                push!(algorithm, "Algorithm $(k)")
+            else
+                push!(algorithm, "$(algname[k])")
+            end
+            append_data!(container, trace)
+        end
     end
 
-    DataFrame(
+    return DataFrame(
         algorithm = algorithm,
-        iterations = iterations,
-        loss = loss,
-        distance = distance,
-        objective = objective,
-        gradient = gradient,
+        iterations = container[1],
+        loss = container[2],
+        distance = container[3],
+        objective = container[4],
+        gradient = container[5],
     )
 end
